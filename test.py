d64cdd0cb210548bf439f084a5c730eea0301b9e
Abstract Potato <21150449+abstractpotato@users.noreply.github.com>
2022-02-01 15:27:20 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,8 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS, cross_origin
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-CORS(app, support_credentials=True)
 
 @app.route('/', methods=['GET'])
-@cross_origin(supports_credentials=True)
 def puzzle():
     response = {"running": "True"}
     return jsonify(response), 200
